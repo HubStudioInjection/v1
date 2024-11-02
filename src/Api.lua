3eba232e74e7cabbd67507ef39227a6a8f49e855
HubStudioInjection <hubstudioijection@gmail.com>
2024-11-02 19:36:09 +0200

    Add files via upload

---
+++ src/Api.lua
@@ -0,0 +1,20 @@
+local Api = true
+if Api == true then
+local Root = {}
+local Part = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
+end)
+
+local Hum = {}
+local Part = {} game:GetService("Players").LocalPlayer.Character.Humanoid
+end)
+
+local function() = {}
+local Part = {} game:function()
+end)
+   return Root 
+  return Part
+ return Hum 
+  return Part
+return function()
+  return Part
+end
