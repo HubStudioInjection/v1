1d9dc4590e3fada145fc034533d3c292b90b873d
HubStudioInjection <hubstudioijection@gmail.com>
2024-11-03 10:39:43 +0200

    Update Get.lua

--- Get.lua
+++ Get.lua
@@ -1,12 +1,3 @@
-local Get = true
-if Get == true then
-getgenv().local.Api = loadstring(game:HttpGet(".. scr/Api.lua .."))()
-  getgenv().local.Api.Parent = loadstring(game:HttpGet(".. Protogen.Genic Example.lua .."))()
-     getgenv().local.ESP.Source = loadstring(game:HttpGet(".. ESP .."))()
-       getgenv().local.ESP.Parent = loadstring(game:HttpGet(".. Protogen.Genic Example.lua .."))()
-         getgenv().local.Tracers.Source = loadstring(game:HttpGet(".. Tracers .."))()
-           getgenv().local.Tracers.Parent = loadstring(game:HttpGet(".. Protogen.Genic Example.lua .."))()
-end
 while true do
    makeScript
 end
